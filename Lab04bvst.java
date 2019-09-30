7936e39f6668271850fb854204e928b3ab6a60b1
Terzakis <10951@vtsd.com>
2019-09-30 08:43:56 -0400

    first draft

--- Lab04bvst.java
+++ Lab04bvst.java
@@ -13,12 +13,16 @@ public class Lab04bvst extends Applet
 	public void paint(Graphics g)
 	{
 		// DRAW CUBE
-
-
+		g.drawRect(100,100,200,200);
+		g.drawRect(175,175,200,200);
+		g.drawLine(100,100,175,175);
+		g.drawLine(300,100,375,175);
+		g.drawLine(100,300,175,375);
+		g.drawLine(300,300,375,375);
 
 		// DRAW SPHERE
-
-
+		g.drawOval(137.5,137.5,200,200);
+		g.drawOval(137.5,137.5,200,200);
 
 		// DRAW INSCRIBED/CIRCUMSCRIBED TRIANGLE
 
