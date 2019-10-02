b41d771c8f34a8d4f844b93922fbdbd1eff9938d
Terzakis <10951@vtsd.com>
2019-10-02 08:44:25 -0400

    Day three

--- Lab04bvst.java
+++ Lab04bvst.java
@@ -31,12 +31,30 @@ public class Lab04bvst extends Applet
 
 		// DRAW INSCRIBED/CIRCUMSCRIBED TRIANGLE
 		g.drawOval(600,100,200,200);
-		g.drawLine(600,200,800,150);
-
+		g.drawLine(600,200,785,150);
+		g.drawLine(700,300,785,150);
+		g.drawLine(600,200,700,300);
+		g.drawOval(644,178,92,92);
 
 		// DRAW APCS
-
-
+		//A
+		g.fillRect(150,400,25,100);
+		g.fillRect(150,400,75,25);
+		g.fillRect(200,400,25,100);
+		g.fillRect(150,440,75,25);
+		//P
+		g.fillRect(250,400,25,100);
+		g.fillRect(250,400,75,25);
+		g.fillRect(300,400,25,50);
+		g.fillRect(250,440,75,25);
+		//C
+		g.fillRect(350,400,25,100);
+		g.fillRect(350,400,75,25);
+		g.fillRect(350,475,75,25);
+		//S
+		g.fillRect(450,400,25,60);
+		g.fillRect(450,435,75,25);
+		g.fillRect(450,475,75,25);
 
 		// DRAW PACMEN FLOWER
 
